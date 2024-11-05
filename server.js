bb9031db95967b6ff09ad714c6e7d7f2c5f69d66
willy-gurning <willygurning14@gmail.com>
2024-11-05 11:58:09 +0700

    Done

--- server.js
+++ server.js
@@ -76,10 +76,10 @@ app.get('/download', (req, res) => {
     });
 });
 
-app.listen(PORT, (err) => {
+app.listen(port, (err) => {
     if (err) {
         console.error('Failed to start server:', err);
     } else {
-        console.log(`Server running on port ${PORT}`);
+        console.log(`Server running on port ${port}`);
     }
 });
